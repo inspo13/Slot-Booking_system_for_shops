77f728c7b69b737decedf433b6058ef21b782924
Prachi Bhatt <66836154+inspo13@users.noreply.github.com>
2023-09-28 11:49:13 +0530

    project folder added

---
+++ Slot-Booking_system_for_shops/SLOT BOOKING SYSTEM.sql
@@ -0,0 +1,15 @@
+#contribution:Prachi Bhatt
+use slotbookingsystem;
+CREATE TABLE slot1_10AM_to_12AM(TokenNo INT(3), Name VARCHAR(20),
+       PhoneNo INT(10), date date); 
+
+CREATE TABLE slot2_1PM_to_3PM(TokenNo INT(3), Name VARCHAR(20),
+       PhoneNo INT(10), date date);  
+
+CREATE TABLE slot3_4PM_to_6PM(TokenNo INT(3), Name VARCHAR(20),
+       PhoneNo INT(10), date date); 
+
+CREATE TABLE slot4_7PM_to_9PM(TokenNo INT(3), Name VARCHAR(20),
+       PhoneNo INT(10), date date);
+
+
